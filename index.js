972508405aceb22df0e040cdf412d4ecefad6225
rmanselefi <kebederaz@gmail.com>
2023-06-01 21:08:21 +0300

    added google auth

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 const express = require("express");
 
+const passport = require("passport");
+const keys = require("./config/keys");
+
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientId,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken) => {
+      console.log(accessToken);
+    }
+  )
+);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'))
 
 const PORT = process.env.PORT || 5000;
 
